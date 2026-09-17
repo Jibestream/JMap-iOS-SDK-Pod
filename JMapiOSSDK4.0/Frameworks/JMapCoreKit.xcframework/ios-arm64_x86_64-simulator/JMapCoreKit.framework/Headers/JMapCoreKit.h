a39eb55577618d125db7e8c17a6e584b28803fae
sajit-cxapp <v-sajit.nair@cxapp.com>
2026-09-18 01:39:06 +0530

    5.13.2 manual pod upload

--- JMapiOSSDK4.0/Frameworks/JMapCoreKit.xcframework/ios-arm64_x86_64-simulator/JMapCoreKit.framework/Headers/JMapCoreKit.h
+++ JMapiOSSDK4.0/Frameworks/JMapCoreKit.xcframework/ios-arm64_x86_64-simulator/JMapCoreKit.framework/Headers/JMapCoreKit.h
@@ -64,5 +64,5 @@
 #import <JMapCoreKit/JMapPathPerFloor.h>
 #import <JMapCoreKit/JMapPoint.h>
 
-#define JMapSDK_VERSION_STRING @"5.13.1"
+#define JMapSDK_VERSION_STRING @"5.13.2"
 #define JMapSDK_DEFAULT_TARGET_SERVER_VERSION @"4.12"
